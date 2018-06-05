b1951b42ec8f7772dfa084f6d7696de140c7b661
Sakif Fahmid Zaman <smfzaman@gmail.com>
2018-06-05 14:29:58 -0300

    added deque

--- testList.cpp
+++ testList.cpp
@@ -1,17 +1,34 @@
-#include "Double_sentinel_list.h"
+#include "Double_sentinel_list.hpp"
 
 using namespace std;
 
 int main() {
-	cout << "Begin Test\n";
-	Double_sentinel_list<int> *list = new Double_sentinel_list<int>();
-	cout << "List Size: " << list->size() << endl;
-	cout << "List is empty: " << list->empty() << endl;
+	auto list = Double_sentinel_list<int>();
+	cout << "List size: " << list.size() << endl;
+	cout << "List empty: " << list.empty() << endl;
 	try {
-		list->front();
+		list.front();
 	} catch (underflow) {
-		cout << "Underflow caught\n";
+		cout << "Underflow Caught!\n";
 	}
-	cout << "End Test\n";
+	try {
+		list.back();
+	} catch (underflow) {
+		cout << "Underflow Caught!\n";
+	}
+	try {
+		list.pop_back();
+	} catch (underflow) {
+		cout << "Underflow Caught!\n";
+	}
+	try {
+		list.pop_front();
+	} catch (underflow) {
+		cout << "Underflow Caught!\n";
+	}
+	list.push_front(7);
+	list.push_back(12);
+	cout << "List size: " << list.size() << endl;
+	cout << "List empty: " << list.empty() << endl;
 	return 0;
 }
