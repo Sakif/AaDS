23681bc86e631dd3e56707f4ea1c5cb08b973ef1
Sakif Zaman <smfzaman@gmail.com>
2021-04-21 00:22:54 -0300

    array

--- header/array.hpp
+++ header/array.hpp
@@ -1,26 +1,31 @@
 #pragma once
+#include <algorithm>
+#include <array>
+#include <cmath>
+#include <stdexcept>
+
 #ifndef ARRAY_HPP
 #define ARRAY_HPP
 
-#include <algorithm>
-
 namespace AaDS {
 
-class array
-{
+class array {
 private:
   unsigned array_capacity;
-  int* internal_array;
+  int *internal_array;
   unsigned array_size;
 
 public:
   array(int = 10);
+  array(array const &);
+  ~array();
 
   /* Accessors */
   unsigned size() const;
   unsigned capacity() const;
   bool empty() const;
   bool full() const;
+  int operator[](unsigned n) const;
 
   /* statistical functions */
   unsigned sum() const;
@@ -31,6 +36,7 @@ public:
   /* Mutators */
   bool append(int);
   void clear();
+  void swap(array &other);
 };
 
 /* Constructor
@@ -40,42 +46,60 @@ public:
  * The array is initially empty, and there is no need to zero out the entries of the array;
  */
 array::array(int n)
-  : array_capacity(std::max(1, n))
-  , internal_array(new int[array_capacity])
-  , array_size(0)
-{}
+    : array_capacity(std::max(1, n)),
+      internal_array(new int[array_capacity]),
+      array_size(0) {}
 
-unsigned
-array::size() const
-{
+/* copy constructor */
+array::array(array const &o)
+    : array_capacity(o.array_capacity),
+      internal_array(new int[array_capacity]),
+      array_size(o.array_size) {
+  /* copy all the entities from the other array */
+  for (auto i = 0U; i < size(); i++) {
+    internal_array[i] = o.internal_array[i];
+  }
+}
+
+/* distructor */
+array::~array() {
+  delete[] internal_array;
+}
+
+/* returns the size of the array */
+unsigned array::size() const {
   return array_size;
 }
 
-bool
-array::empty() const
-{
+/* shows if the array is empty or not */
+bool array::empty() const {
   return (size() == 0);
 }
 
-void
-array::clear()
-{
+/* clears the array by setting the array_size to 0 */
+void array::clear() {
   array_size = 0;
 }
 
-bool
-array::append(int n)
-{
-  if (array_size >= array_capacity) {
+/* shows the capacity of the array */
+unsigned array::capacity() const {
+  return array_capacity;
+}
+
+/* adds item to the end of the array */
+bool array::append(int n) {
+  if (size() >= capacity()) {
+    /* cannot append if array is full */
     return false;
   }
-  internal_array[array_size++] = n;
+
+  internal_array[array_size] = n;
+  array_size++;
   return true;
 }
 
-unsigned
-array::sum() const
-{
+/* adds all the items in the array and returns the sum */
+unsigned array::sum() const {
   auto sum = 0U;
   for (auto i = 0U; i < size(); i++) {
     sum += internal_array[i];
@@ -83,5 +107,45 @@ array::sum() const
   return sum;
 }
 
+/* calculates the average of the array */
+double array::average() const {
+  if (size() < 1) {
+    throw std::underflow_error("Average requires at least 1 item");
+  }
+  return static_cast<double>(sum()) / static_cast<double>(size());
 }
+
+/* calculates the sample veriance of all the items in the array */
+double array::variance() const {
+  if (size() < 2) {
+    throw std::underflow_error("Variance requires at least 2 items");
+  }
+  auto exp = average();
+  auto var = 0.0;
+
+  for (auto i = 0U; i < size(); i++) {
+    var += pow(internal_array[i] - exp, 2.0);
+  }
+
+  return var / (size() - 1);
+}
+
+/* calculates the standerd deviation of the array */
+double array::std_dev() const {
+  return sqrt(variance());
+}
+
+int array::operator[](unsigned n) const {
+  return internal_array[n];
+}
+
+/* swap */
+void array::swap(array &other) {
+  std::swap(array_capacity, other.array_capacity);
+  std::swap(internal_array, other.internal_array);
+  std::swap(array_size, other.array_size);
+}
+
+} // namespace AaDS
+
 #endif
--- main.cpp
+++ main.cpp
@@ -1,13 +1,40 @@
 #include "header/array.hpp"
 #include <cstdlib>
 #include <iostream>
+#include <ostream>
 
 using namespace std;
 
-int
-main()
-{
-  AaDS::array info(10);
+int main() {
+  auto info = AaDS::array(30);
+
+  for (auto i = 0U; i < info.capacity(); i++) {
+    auto r = rand() % 100;
+    info.append(r);
+  }
+
+  for (auto i = 0U; i < info.size(); i++) {
+    cout << info[i] << ", ";
+  }
+
+  cout << endl
+       << "Sum: " << info.sum() << endl
+       << "Average: " << info.average() << endl
+       << "Variance: " << info.variance() << endl
+       << "Std Dev: " << info.std_dev() << endl;
+
+  AaDS::array a(3), b(5);
+  a.append(54);
+  a.append(25);
+  a.append(37);
+  b.append(92);
+  b.append(82);
+  a.swap(b);
+
+  cout << "a size: " << a.size() << endl
+       << "a capacity: " << a.capacity() << endl
+       << "b size: " << b.size() << endl
+       << "b capacity: " << b.capacity() << endl;
 
   return EXIT_SUCCESS;
 }
