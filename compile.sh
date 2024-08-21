1816e49e0ae872b82ac5321b4bc1dc5a3e224785
Sakif Zaman <smfzaman@gmail.com>
2024-08-21 07:39:41 -0400

    resume

--- compile.sh
+++ compile.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/env bash
 
 dir="$PWD/build/"
+
 if [ ! -d "$dir" ];then
     cmake -G Ninja -S $PWD -B $dir -DCMAKE_EXPORT_COMPILE_COMMANDS=1
 fi
 
 mold -run cmake --build $dir
 
-./build/wee
-#latexmk -pdf resume/resume.tex
-#latexmk -c resume/resume.tex
+# ./build/wee
+latexmk -pdf resume/resume.tex
+latexmk -c resume/resume.tex
